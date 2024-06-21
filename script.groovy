7845d657053535dab947c2b627233ebc1a40619d
Bhuvaneshwar-PH <160835317+Bhuvaneshwar-PH@users.noreply.github.com>
2024-06-21 13:01:10 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,5 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
-} 
-
-def deployApp() {
-    echo 'deploying the application...'
-} 
+def gitpull(){
+    git branch: 'main', url: 'https://github.com/Bhuvaneshwar-PH/java-application.git'
+}
 
 return this
